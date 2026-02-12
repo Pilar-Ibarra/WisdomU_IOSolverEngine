7d3e9f504bd63c71b3e0531df391e28c7fdd0381
Pilar Ibarra <pilaribarra173@gmail.com>
2026-02-12 10:04:31 -0600

    Enhance Solver class with objective and feasibility method documentation

--- solvers/solverAbc.py
+++ solvers/solverAbc.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 class Solver(ABC):
     @abstractmethod
-    def objective(self,x):
+    def objective(self,x): #dado un vector x ¿cuanto gano? cada solver va a tener su propia implementacion de esta funcion
         pass
+    def maxtomin(self,x):#en dado caso queramos minimizar en vez de maximizar, esta funcion convierte el problema de maximizar a minimizar
+        return -self.objective(x)
     @abstractmethod
-    def is_feasible(self,x):
+    def is_feasible(self,x):#cada solver tiene sus reglas para determinar si una solucion es factible o no
         pass
     

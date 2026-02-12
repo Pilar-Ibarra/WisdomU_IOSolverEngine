2628a0dd93cab42976692bd4444e4c5cbefacbef
Pilar Ibarra <pilaribarra173@gmail.com>
2026-02-12 10:03:45 -0600

    Refactor GraphicMetod class structure and methods for linear model integration

--- solvers/graphic_Metodo.py
+++ solvers/graphic_Metodo.py
@@ -1,13 +1,24 @@
 from typing import List
-class grafico_Metodo:
-    def __init__(self):
-        self.__init____(self,consumos: List[List[float]],disponibilidad: List[float],ganancia: List[float]]):
-        def verificar_2_variables(self,x):
-            
-        def convertir_restricciones_a_rectas(self):
-        def calcular_pendientes(self):
-        def calcular_ordenadas_al_origen(self):
-        def calcular_puntos_de_interseccion(self):
-        def filtrar_puntos_validos(self):
-        def calcular_funcion_objetivo_en_puntos_validos(self):
-        def encontrar_solucion_optima(self):
+from .solverAbc import Solver
+from Models.lineal_model import lineal_model
+class GraphicMetod(Solver):
+      def __init__(self,model:lineal_model):
+        self.model=model
+      def two_desitions (self,x):
+        if len(x) != 2:
+            raise ValueError("El problema debe tener solo 2 variables")
+      def convertir_restricciones_a_rectas(self):
+        pass
+      def calcular_pendientes(self):
+        pass
+      def calcular_ordenadas_al_origen(self):
+        pass
+      def calcular_puntos_de_interseccion(self):
+        pass
+      def filtrar_puntos_validos(self):
+          pass
+      def calcular_funcion_objetivo_en_puntos_validos(self):
+          pass
+#investigar convexos
+      def encontrar_solucion_optima(self):
+         pass

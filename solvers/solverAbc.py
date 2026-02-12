0cdcc9b4dae3434c226536917c08e0f7ed28b7fb
Pilar Ibarra <pilaribarra173@gmail.com>
2026-02-12 17:59:50 -0600

    Implement objective method in Solver class and add abstract solve method

--- solvers/solverAbc.py
+++ solvers/solverAbc.py
@@ -2,10 +2,12 @@ from abc import ABC, abstractmethod
 class Solver(ABC):
     @abstractmethod
     def objective(self,x): #dado un vector x ¿cuanto gano? cada solver va a tener su propia implementacion de esta funcion
-        pass
+        return self.model.objective(x)
     def maxtomin(self,x):#en dado caso queramos minimizar en vez de maximizar, esta funcion convierte el problema de maximizar a minimizar
         return -self.objective(x)
     @abstractmethod
     def is_feasible(self,x):#cada solver tiene sus reglas para determinar si una solucion es factible o no
         pass
-    
+    @abstractmethod
+    def solve(self):
+        pass

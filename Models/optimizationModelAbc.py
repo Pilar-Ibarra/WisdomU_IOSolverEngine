e34bee9a4781579ed83122629212a6e21560e8e3
Pilar Ibarra <pilaribarra173@gmail.com>
2026-02-17 11:56:11 -0600

    Refactor OptimizationModel class to use List type hinting and improve method definitions

--- Models/optimizationModelAbc.py
+++ Models/optimizationModelAbc.py
@@ -1,26 +1,35 @@
+import json
 from abc import ABC, abstractmethod
-from typing import list
+from typing import List
 class OptimizationModel(ABC):
-    def __init__(self, n: int):
-        self.n = n
+    def __init__(self,nombre):
+        self.nombre=nombre
+        self.nVariables=[]
+        self.tipo_modelo=""
     @abstractmethod
-    def objective_function(self,x:list[float])->float:
+    def to_json(self,archive):
+              #metodo abstracto para guardar el modelo en un archivo json
+        pass
+    def dataMatrix(self):
         pass
     @abstractmethod
-    def get_objective_coefficients(self) -> list[float]:
+    def objective_function(self,x:List[float])->float:
+        pass
+    @abstractmethod
+    def get_objective_coefficients(self) -> List[float]:
         pass
 
     @abstractmethod
-    def get_constraint_matrix(self) -> list[list[float]]:
+    def get_constraint_matrix(self) -> List[List[float]]:
         pass
 
     @abstractmethod
-    def get_rhs_vector(self) -> list[float]:
+    def get_rhs_vector(self) -> List[float]:
         pass
     @abstractmethod
-    def check_constraints(self,x:list[float])->bool:
+    def check_constraints(self,x:List[float])->bool:
         pass
-    def check_non_negativity(self,x:list[float])->bool:
+    def check_non_negativity(self,x:List[float])->bool:
         for i in range(self.n):
             if x[i]<0:
                 return False

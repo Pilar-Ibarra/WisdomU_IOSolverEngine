56251ace44b29d2cf1241c6dbfb3496d0c82bc56
Pilar Ibarra <pilaribarra173@gmail.com>
2026-02-18 12:52:07 -0600

    archive change for file

--- Models/optimizationModelAbc.py
+++ Models/optimizationModelAbc.py
@@ -7,10 +7,10 @@ class OptimizationModel(ABC):
         self.nVariables=[]
         self.type_model=""
     @abstractmethod
-    def to_json(self,archive):
+    def to_json(self,file):
               #metodo abstracto para guardar el modelo en un archivo json
         pass
-    def from_json(self,archive):
+    def from_json(self,file):
         pass #metodo para cargar el modelo desde un archivo json.
     #getters
     @abstractmethod

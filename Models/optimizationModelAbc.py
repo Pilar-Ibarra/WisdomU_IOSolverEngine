15cc1d313f46f09bc9cf2bb71bfcebe74befa938
Pilar Ibarra <pilaribarra173@gmail.com>
2026-04-20 11:30:14 -0600

    refactor: update dimension validation and clean up abstract methods

--- Models/optimizationModelAbc.py
+++ Models/optimizationModelAbc.py
@@ -7,14 +7,6 @@ class OptimizationModel(ABC):
         self.nVariables=[]
         self.type_model=""
     @abstractmethod
-    def to_json(self,file):
-              #metodo abstracto para guardar el modelo en un archivo json
-        pass
-    @abstractmethod
-    def from_json(self,file):
-        pass #metodo para cargar el modelo desde un archivo json.
-    #getters
-    @abstractmethod
     def objective_function(self,x:List[float])->float:
         pass
     @abstractmethod
